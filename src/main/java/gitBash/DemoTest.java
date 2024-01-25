bd9fa9e0aedd16c02bea1af08faaac962d18fb5e
pragathi-31 <vpragathi99@gmail.com>
2024-01-25 16:42:46 +0530

    4th commit

--- src/main/java/gitBash/DemoTest.java
+++ src/main/java/gitBash/DemoTest.java
@@ -6,8 +6,9 @@ public class DemoTest {
 	@Test
 	public void demoOne() {
 		System.out.println("testOne executing");
-		
-		
-		
+	}
+	@Test
+	public void demotwo() {
+		System.out.println("two executing");
 	}
 }
